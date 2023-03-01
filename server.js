e2fe61a04b3c09b7242b393269e463b2bd5afba3
Rcan3624 <55251953+Rcan3624@users.noreply.github.com>
2023-03-01 18:04:53 -0500

    Updated files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// TODO: Find out what is causing the "Model.find() no longer accepts a callback" error when starting the server
+
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
@@ -5,23 +7,14 @@ const ejs = require('ejs')
 
 app.set('view engine', 'ejs');
 
+// Connect to Group 2 database
 mongoose.connect('mongodb+srv://grouptwo:schoolyard@group2.50g5lu7.mongodb.net/?retryWrites=true&w=majority');
 
 // Code explanation (Timeline 9:46): https://youtu.be/yH593K9fYvE?t=586
 const courseSchema = ({
-    _id: {
-        type: Number,
-        required: true
-    },
-
-    dept: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    }
+    id: Number,
+    dept: String,
+    name: String
 
 });
 
@@ -29,14 +22,15 @@ const Course = mongoose.model('Course', courseSchema);
 
 // Code explanation (Timeline 11:12): https://youtu.be/yH593K9fYvE?t=722
 app.get('/', (req, res) => {
-    Course.find({}, async(err, classes) => {
+    Course.find({}, async(err, classes) => {                  // Code explanation (Timeline 14:45): https://youtu.be/yH593K9fYvE?t=885
         res.render('index', {
             courseList: classes
         })
     })
 })
 
-app.listen(4000, function() {
+// Go to: http://localhost:3000
+app.listen(3000, function() {
     console.log('server is running');
 })
 
